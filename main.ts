788faf9b4090ce3320ef72b7ea53911b445b23cd
Evan-Beaudoin <71400127+Evan-Beaudoin@users.noreply.github.com>
2020-10-16 17:34:18 -0400

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,7 @@
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    basic.pause(1000)
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    basic.pause(1000)
 })
